4d4494555687b864455708dc8a62f1c8803d41bd
farmarmartin <martin.job2906@gmail.com>
2023-02-04 10:12:36 +0100

    update

--- source/authorization/assymetric_keys.php
+++ source/authorization/assymetric_keys.php
@@ -19,12 +19,8 @@
     $keys = (new Keys)->getKeys();
 
     $SQL = "INSERT INTO user_details (id, full_name, public_key) VALUES ($identifier, '$user','$keys->public_key');";
-    $db = (new mysqli("localhost", "root", "", "digital_signature"))->sendQuery($SQL);
-
     mysqli_query($connect, $SQL);
-
     mysqli_close($connect);
-
 
     echo "<textarea rows='29' cols='60'>".$id.$keys->private_key."</textarea>";
     ?>
